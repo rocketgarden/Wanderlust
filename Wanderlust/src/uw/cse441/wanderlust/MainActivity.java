e21ee56c8f1d31336018bec4d525e77baf880197
cronus91 <blasv@uw.edu>
2013-05-08 14:57:20 -0800

    Purged main activity, added some strings

--- Wanderlust/src/uw/cse441/wanderlust/MainActivity.java
+++ Wanderlust/src/uw/cse441/wanderlust/MainActivity.java
@@ -1,18 +1,8 @@
 package uw.cse441.wanderlust;
 
-import java.util.Locale;
-
-import android.os.Bundle;
 import android.app.Activity;
-import android.app.Fragment;
-import android.app.FragmentManager;
-import android.view.Gravity;
-import android.view.LayoutInflater;
+import android.os.Bundle;
 import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.TextView;
 
 /**
  * This is the main, and probably only activity for the application.
@@ -21,25 +11,9 @@ import android.widget.TextView;
  * @author Vince
  *
  */
-@SuppressWarnings("unused")
 public class MainActivity extends Activity{
 
-	/**
-	 * The {@link android.view.PagerAdapter} that will provide
-	 * fragments for each of the sections. We use a
-	 * {@link android.app.FragmentPagerAdapter} derivative, which
-	 * will keep every loaded fragment in memory. If this becomes too memory
-	 * intensive, it may be best to switch to a
-	 * {@link android.app.FragmentStatePagerAdapter}.
-	 */
-
-	/**
-	 * The {@link ViewPager} that will host the section contents.
-	 */
 	
-	
-	private static final int NUM_PAGES = 5;
-
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
