c96b88dccd36b23587fbb5f7214e3d85c79fd582
cronus91 <blasv@uw.edu>
2013-05-10 11:24:40 -0800

    Added location tracking and permissions

--- Wanderlust/src/uw/cse441/wanderlust/Map_Fragment.java
+++ Wanderlust/src/uw/cse441/wanderlust/Map_Fragment.java
@@ -2,6 +2,9 @@ package uw.cse441.wanderlust;
 
 import android.location.Location;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -18,10 +21,22 @@ public class Map_Fragment extends MapFragment {
 		currentLocation = new Location("default");
 		currentLocation.setLatitude(47.671369);
 		currentLocation.setLongitude(-122.342603);
+		
+		
+		//TODO Add map markers here
+	}
+	
+	@Override
+	public View onCreateView (LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
+		View view = super.onCreateView(inflater, container, savedInstanceState);
+		getMap().setMyLocationEnabled(true);
+		return view;
+		
 	}
 
 	public void onPause() {
 		currentLocation = getMap().getMyLocation();
+		
 	}
 
 	@Override
