74cd712c569d83fe6ea55df4f1c0b2ee03408493
Brandon Bond <bond13@uw.edu>
2013-05-15 13:30:50 -0700

    Revert "added onclick code"
    This reverts commit 940c77e3f7ca6d85dd4a7c42012f8c36b8114561.

--- Wanderlust/src/uw/cse441/wanderlust/Map_Fragment.java
+++ Wanderlust/src/uw/cse441/wanderlust/Map_Fragment.java
@@ -76,7 +76,6 @@ public class Map_Fragment extends MapFragment implements OnMarkerClickListener {
 	        getMap().addMarker(new MarkerOptions().position(loc)
 	  	          .title('m' + Integer.toString(m.getId())));
 		}
-		getMap().setOnMarkerClickListener(this);
 	}
 	
 	@Override
