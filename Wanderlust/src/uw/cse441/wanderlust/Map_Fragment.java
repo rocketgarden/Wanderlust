46af442f2e0e46013e4155314ca2296ab0f67503
blasv <blas.vince@gmail.com>
2013-05-19 10:42:02 -0700

    Merged in changes and set Map Fragment to use substrings instead of charAt()s

--- Wanderlust/src/uw/cse441/wanderlust/Map_Fragment.java
+++ Wanderlust/src/uw/cse441/wanderlust/Map_Fragment.java
@@ -89,13 +89,13 @@ public class Map_Fragment extends MapFragment implements OnMarkerClickListener {
 		if (title.charAt(0) == 'm') { // marker represents a marker
 			Button meetupButton = (Button) getActivity().findViewById(R.id.meetUpButton);
 			meetupButton.setVisibility(View.GONE);
-			Meetup m = MainActivity.getPlaceDataProvider().getMeetup(Character.getNumericValue(title.charAt(1)));
+			Meetup m = MainActivity.getPlaceDataProvider().getMeetup(Integer.parseInt(title.substring(1)));
 			((TextView) getActivity().findViewById(R.id.topBar_address)).setText(m.getAddress());
 			((TextView) getActivity().findViewById(R.id.topBar_title)).setText(m.getTitle());
 		} else { // marker represents a poi
 			Button meetupButton = (Button) getActivity().findViewById(R.id.meetUpButton);
 			meetupButton.setVisibility(View.VISIBLE);
-			POI p = MainActivity.getPlaceDataProvider().getPOI(Character.getNumericValue(title.charAt(1)));
+			POI p = MainActivity.getPlaceDataProvider().getPOI(Integer.parseInt(title.substring(1)));
 			((TextView) getActivity().findViewById(R.id.topBar_address)).setText(p.getAddress());
 			((TextView) getActivity().findViewById(R.id.topBar_title)).setText(p.getTitle());
 		}
