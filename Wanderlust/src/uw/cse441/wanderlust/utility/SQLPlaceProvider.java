d196defdd2711ea1ef338cf16687f3259a1ee750
blasv <blas.vince@gmail.com>
2013-05-19 21:28:22 -0700

    Refactored database population code into BasicPlaceProvider

---
+++ Wanderlust/src/uw/cse441/wanderlust/utility/SQLPlaceProvider.java
@@ -0,0 +1,55 @@
+package uw.cse441.wanderlust.utility;
+
+import java.util.List;
+
+public class SQLPlaceProvider implements PlaceDataProvider {
+
+	@Override
+	public POI getPOI(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<POI> getPOIList() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Meetup getMeetup(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Meetup> getMeetupList() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void addPOI(POI poi) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void addMeetup(Meetup meetup) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public int getNextPoiId() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int getnextMeetupId() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
